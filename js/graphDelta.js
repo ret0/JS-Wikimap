6fcc6e6911e289d458b8ea08e6af6c86611de575
Reto Kleeb <retokl@gmail.com>
2011-06-01 11:42:30 -0400

    cleaned up js file mess

---
+++ js/graphDelta.js
@@ -0,0 +1,447 @@
+var frameInformation = {
+    1: {
+        add: [{
+            nodeName: "Prince"
+        },
+        {
+            nodeName: "Nerina Pallot"
+        },
+        {
+            nodeName: "Giovanni Zarrella"
+        },
+        {
+            nodeName: "Albano Carrisi"
+        },
+        {
+            nodeName: "Jennifer Hudson"
+        },
+        {
+            nodeName: "Stéphanie von Monaco"
+        },
+        {
+            nodeName: "Diana Ross"
+        },
+        {
+            nodeName: "Mariah Carey"
+        },
+        {
+            nodeName: "Adam Lambert"
+        }],
+        del: [{
+            nodeName: "Janet Jackson"
+        }],
+        links: [{
+            source: "Taio Cruz",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "La Toya Jackson",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Den Harrow",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Kim Wilde",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Patti Austin",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Darren Hayes",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Natalie Gauci",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Harry Belafonte",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Cher (Künstlerin)",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Bill Withers",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Michael Sembello",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Fancy",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Silvy de Bie",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Randy Jackson",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Marc Terenzi",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Tito Jackson",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Rebbie Jackson",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Pia Zadora",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Lionel Richie",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Madonna (Künstlerin)",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Sarah Connor",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Lisa Marie Presley",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Nerina Pallot",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Prince",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Jarvis Cocker",
+            target: "Michael Jackson",
+            value: 2
+        },
+        //{source: "Janet Jackson", target: "Michael Jackson", value: 2},
+        {
+            source: "Céline Dion",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Tomomi Kahala",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Kenny Loggins",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Zeeteah",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Bob Gaudio",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "DJ BoBo",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "DJ BoBo",
+            target: "Céline Dion",
+            value: 2
+        },
+        {
+            source: "Sharon Cuneta",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Mai Kuraki",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Nisha Kataria",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Jermaine Jackson",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Stevie Wonder",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Rockwell (Musiker)",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "David Bowie",
+            target: "Michael Jackson",
+            value: 2
+        }]
+    },
+    2: {
+        add: [{
+            nodeName: "TEST 2nd Frame"
+        }],
+        del: [],
+        links: [{
+            source: "Taio Cruz",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "La Toya Jackson",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Den Harrow",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Kim Wilde",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Patti Austin",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Darren Hayes",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Natalie Gauci",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Harry Belafonte",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Cher (Künstlerin)",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Bill Withers",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Michael Sembello",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Fancy",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Silvy de Bie",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Randy Jackson",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Marc Terenzi",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Tito Jackson",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Rebbie Jackson",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Pia Zadora",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Lionel Richie",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Madonna (Künstlerin)",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Sarah Connor",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Lisa Marie Presley",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Nerina Pallot",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Prince",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Jarvis Cocker",
+            target: "Michael Jackson",
+            value: 2
+        },
+        //{source: "Janet Jackson", target: "Michael Jackson", value: 2},
+        {
+            source: "Céline Dion",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Tomomi Kahala",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Kenny Loggins",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Zeeteah",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Bob Gaudio",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "DJ BoBo",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "DJ BoBo",
+            target: "Céline Dion",
+            value: 2
+        },
+        {
+            source: "Sharon Cuneta",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Mai Kuraki",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Nisha Kataria",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Nisha Kataria",
+            target: "Céline Dion",
+            value: 2
+        },
+        {
+            source: "Jermaine Jackson",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Jermaine Jackson",
+            target: "Céline Dion",
+            value: 2
+        },
+        {
+            source: "Stevie Wonder",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "Rockwell (Musiker)",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "David Bowie",
+            target: "Michael Jackson",
+            value: 2
+        },
+        {
+            source: "TEST 2nd Frame",
+            target: "Céline Dion",
+            value: 2
+        }]
+    }
+};
